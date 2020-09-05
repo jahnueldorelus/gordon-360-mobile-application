389881028ec85a8718d6a046cefc2ccef38564b0
jahnueldorelus <jahnueldorelus@gmail.com>
2020-09-04 21:46:22 -0400

    Gordon 360 App with navigation and Webview component

--- App.js
+++ App.js
@@ -1,22 +1,53 @@
-import React, { Component } from "react";
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Button,
-} from "react-native";
+// import React, { Component } from "react";
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View,
+//   Dimensions,
+//   Button,
+// } from "react-native";
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { WebView } from "react-native-webview";
 
+// const deviceHeight = Dimensions.get("window").height;
+// const deviceWidth = Dimensions.get("window").width;
+
+// type Props = {};
+// export default class App extends Component<Props> {
+//   render() {
+//     return (
+//       <WebView
+//         style={styles.webview}
+//         source={{ uri: "https://360.gordon.edu" }}
+//         javaScriptEnabled={true}
+//         domStorageEnabled={true}
+//         startInLoadingState={false}
+//         scalesPageToFit={true}
+//       />
+
+//     );
+//   }
+// }
+
+import React from "react";
+import { Button, View, StyleSheet, Dimensions } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
 import { WebView } from "react-native-webview";
 
 const deviceHeight = Dimensions.get("window").height;
 const deviceWidth = Dimensions.get("window").width;
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={navigation.openDrawer} title="Open navigation drawer" />
+      <Button
+        onPress={() => navigation.navigate("Notifications")}
+        title="Go to notifications"
+      />
       <WebView
         style={styles.webview}
         source={{ uri: "https://360.gordon.edu" }}
@@ -25,8 +56,30 @@ export default class App extends Component<Props> {
         startInLoadingState={false}
         scalesPageToFit={true}
       />
-    );
-  }
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={navigation.openDrawer} title="Open navigation drawer" />
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({

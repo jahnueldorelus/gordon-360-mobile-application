e3ded2f0b21d413b9baa042f7bdf3c39349896bb
jahnueldorelus <jahnueldorelus@gmail.com>
2020-11-02 01:18:17 -0500

    Updated Gordon 360 Offline Component 	- Organized styling so that all styles are in one object 	- Redesigned user's information to a card layout style

--- src/Views/Offline360/index.js
+++ src/Views/Offline360/index.js
@@ -50,18 +50,24 @@ export const Offline360 = (props) => {
   const styles = StyleSheet.create({
     mainContaner: {
       flex: 1,
-      //   padding: 20,
       backgroundColor: "white",
     },
-    infoView: { padding: 20 },
+    infoContainer: { paddingHorizontal: 20, paddingTop: 20 },
     introText: { fontSize: 21, alignSelf: "center", marginBottom: 15 },
     introTextName: { color: "#014983" },
     listItemName: {
       fontSize: 18,
+      color: "#acdafe",
     },
     listItemValue: {
-      color: "#014983",
+      color: "white",
       fontSize: 18,
+    },
+    listItemImage: {
+      width: 50,
+      height: 50,
+      marginBottom: 5,
+      tintColor: "white",
     },
     userImage: {
       width: 100,
@@ -71,13 +77,31 @@ export const Offline360 = (props) => {
       marginBottom: 10,
       alignSelf: "center",
     },
+    infoView: {
+      flexDirection: "row",
+      marginBottom: 20,
+      marginHorizontal: 10,
+      width: "100%",
+    },
+    infoViewCard: {
+      alignItems: "center",
+      backgroundColor: "#014983",
+      borderRadius: 10,
+      marginHorizontal: 10,
+      padding: 10,
+      margin: 5,
+      minWidth: 100,
+    },
+    schedule: {
+      paddingBottom: 20,
+    },
   });
 
   if (userProfile && userImage && userDining)
     return (
       <View style={styles.mainContaner}>
         <ScrollView>
-          <View style={styles.infoView}>
+          <View style={styles.infoContainer}>
             {/********************** User Image **********************/}
             <Image
               source={{ uri: `data:image/gif;base64,${userImage}` }}
@@ -91,48 +115,48 @@ export const Offline360 = (props) => {
               </Text>
             </Text>
             {/********************** User ID **********************/}
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Text style={styles.listItemName}>ID #</Text>
-              <Text style={styles.listItemValue}>{userProfile.ID}</Text>
-            </View>
+            <ScrollView horizontal>
+              <View style={styles.infoView}>
+                <View style={styles.infoViewCard}>
+                  <Image
+                    style={styles.listItemImage}
+                    source={require("./Images/id.png")}
+                  />
+                  <Text style={styles.listItemName}>ID</Text>
+                  <Text numberOfLines={1} style={styles.listItemValue}>
+                    {userProfile.ID}
+                  </Text>
+                </View>
 
-            {/********************** Mailbox # **********************/}
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Text style={styles.listItemName}>Mailbox #</Text>
-              <Text style={styles.listItemValue}>
-                {userProfile.Mail_Location}
-              </Text>
-            </View>
+                {/********************** Mailbox # **********************/}
+                <View style={styles.infoViewCard}>
+                  <Image
+                    style={styles.listItemImage}
+                    source={require("./Images/mailbox.png")}
+                  />
+                  <Text style={styles.listItemName}>Mailbox</Text>
+                  <Text numberOfLines={1} style={styles.listItemValue}>
+                    #{userProfile.Mail_Location}
+                  </Text>
+                </View>
 
-            {/********************** Dining Info **********************/}
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Text style={styles.listItemName}>Dining</Text>
-              <Text style={styles.listItemValue}>0</Text>
-            </View>
+                {/********************** Dining Info **********************/}
+                <View style={styles.infoViewCard}>
+                  <Image
+                    style={styles.listItemImage}
+                    source={require("./Images/dining.png")}
+                  />
+                  <Text style={styles.listItemName}>Dining</Text>
+                  <Text numberOfLines={1} style={styles.listItemValue}>
+                    0
+                  </Text>
+                </View>
+              </View>
+            </ScrollView>
           </View>
           {/********************** Scheduling Info **********************/}
           {userSchedule && (
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
+            <View style={styles.schedule}>
               <OfflineSchedule events={userSchedule} />
             </View>
           )}
